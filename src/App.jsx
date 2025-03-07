0fa7402bacad357dd74f439e659f1cc3a773631d
Mfonisorobert <mfonisorobert1@gmail.com>
2025-03-07 14:45:58 +0100

    modifying hero page

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,33 @@ import React from "react";
           </div>
         </header>
 
-            <div className="bg-[#222831] h-[100vh]">
-              hero
+            <div className="bg-[#222831] h-[100vh] flex flex-col justify-center items-start px-40">
+            <h1 className="text-[80px] text-[#FFFFFF] font-extrabold font-poppins"> 
+            CREATIVE WEB <p className="-mt-10 text-[#bf205a]">DEVELOPER</p>
+            </h1>
+            <div className="flex">
+            <button className='bg-[#bf205a] text-white px-10 py-2 rounded-3xl font-medium ml-2 hover:bg-white hover:text-[#bf205a]'>
+              Hire Me
+              </button>
+              <div>
+            <button className='bg-[#ffffff] text-[#bf205a] px-8 py-2  gap-4 rounded-3xl flex font-medium ml-12  hover:bg-[#bf205a] hover:text-[#ffffff]'>
+              Download CV 
+              <img src='/downloadicon.svg' 
+              className="w-5 h-6  invert hover:brightness-0"/></button>
+              
+              </div>
             </div>
+
+            <div className="w-[60%] flex items-center justify-center relative">
+          <img
+            src="/heropics.svg"
+            className="w-[80%]   object-cover absolute -right-72"
+            alt="heropics"
+          />
+        </div>
+            </div>
+            
+            
     </div>
   )
 }
