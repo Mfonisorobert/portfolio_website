b4126baf4a8f11c2b61f1f1fd716cbec9a4473d2
Mfonisorobert <mfonisorobert1@gmail.com>
2025-03-29 18:47:41 +0100

    responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -61,10 +61,10 @@ import React from "react";
         </div>
 
             {/* Second Background */}
-            <div className="w-full md:w-[60%] flex items-center justify-center relative">
+            <div className="w-full md:w-[60%] flex items-center justify-center relative ">
           <img
             src="/heropics.svg"
-            className="w-[90%] md:w-[20%] lg:w-[60%] object-cover md:absolute md:-right-96 md:mb-26 -mt-48 md:-mt-0"
+            className="w-[50%] md:w-[20%] lg:w-[60%] object-cover md:absolute md:-right-96 md:mb-26 -mt-48 md:-mt-0"
             alt="heropics"
           />
         </div>
